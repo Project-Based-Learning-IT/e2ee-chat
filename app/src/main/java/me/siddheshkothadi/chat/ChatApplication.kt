602918f0e01df3fa3ba5f25243ec74120847e6c9
Siddhesh Kothadi <realsiddheshko@gmail.com>
2022-04-11 23:07:52 +0530

    Remove all files

--- app/src/main/java/me/siddheshkothadi/chat/ChatApplication.kt
+++
@@ -1,7 +0,0 @@
-package me.siddheshkothadi.chat
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ChatApplication: Application() {}
--- app/src/main/java/me/siddheshkothadi/chat/domain/model/Message.kt
+++
@@ -1,9 +0,0 @@
-package me.siddheshkothadi.chat.domain.model
-
-data class Message(
-    val from: String = "",
-    val to: String = "",
-    val timestamp: String = "",
-    val content: String = "",
-    val secretKey: String = ""
-)
